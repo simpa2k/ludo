8113a01523ac13f5872b2537ecbe4a06a6bfe10f
Simon Olofsson <s.olofsson@gmail.com>
2016-11-04 10:38:44 +0100

    Added game piece class.

---
+++ src/gamePiece/GamePiece.java
@@ -0,0 +1,16 @@
+package gamePiece;
+
+import javax.swing.*;
+import java.awt.Color;
+
+public class GamePiece extends JComponent {
+
+    public static final long serialVersionUID = 1L;
+
+    private Color color;
+
+    public GamePiece() {
+
+    }
+
+}
