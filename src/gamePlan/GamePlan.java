0070599bd8af95b2216d706bb03cdecbaf960b7a
Simon Olofsson <s.olofsson@gmail.com>
2016-11-04 11:11:27 +0100

    Board now draws in a mostly correct way. Still needs proper colouring.

--- src/gamePlan/GamePlan.java
+++ src/gamePlan/GamePlan.java
@@ -8,13 +8,33 @@ public class GamePlan extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
+    private static final int HEIGHT = 11;
+    private int width = 3;
+
     private Place place = new Place(Color.WHITE);
 
     public GamePlan(){
 
-        for(int i = 0; i < 3; i++){
-            place = new Place(Color.BLUE);
-            this.add(place);
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        for (int y = 0; y < HEIGHT; y++) {
+
+            JPanel row = new JPanel(); 
+
+            if(y > 3 && y < 7) {
+                width = 11;
+            } else if(width != 3) {
+                width = 3;
+            }
+            
+            for (int x = 0; x < width; x++) {
+
+                place = new Place(Color.BLUE);
+                row.add(place);
+
+            }
+
+            this.add(row);
         }
     }
 }
