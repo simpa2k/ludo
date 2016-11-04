07cd1b6379240164d4cdacb55ebe6d7ad786e0a7
Simon Olofsson <s.olofsson@gmail.com>
2016-11-04 11:39:28 +0100

    Added Home class.

---
+++ src/place/Home.java
@@ -0,0 +1,13 @@
+package place;
+
+import java.awt.Color;
+
+public class Home extends Place {
+
+    private static final long serialVersionUID = 1L;
+
+    public Home(Color color) {
+        super(color);
+    }
+
+}
