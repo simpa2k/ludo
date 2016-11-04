666fdd2ed4eaaeedee50829e9edad00de00ddc30
Svadilfare <robert.dighem@gmail.com>
2016-11-04 11:22:06 +0100

    Added array for GamePiece in Player

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,10 +1,18 @@
 package player;
 
+import gamePiece.GamePiece;
+
 /**
  * Created by Robert on 2016-11-04.
  */
 public class Player {
 
+    GamePiece[] gamePieceArray = new GamePiece[4];
 
+    public Player() {
+        for (int i = 0; i < gamePieceArray.length; i++) {
+            gamePieceArray[i] = new GamePiece();
+        }
+    }
 
 }
